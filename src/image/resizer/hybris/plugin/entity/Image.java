a3c8a55a69ccc724633106612ce2e963401a792b
Noxolo.Mkhungo <>
2017-03-30 10:44:34 +0200

    Image Resizer

---
+++ src/image/resizer/hybris/plugin/entity/Image.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package image.resizer.hybris.plugin.entity;
+
+/**
+ *
+ * @author Noxolo.Mkhungo
+ */
+public class Image {
+    
+    private int IMG_WIDTH ;
+    private int IMG_HEIGHT ;
+
+    public Image(int IMG_WIDTH, int IMG_HEIGHT) {
+        this.IMG_WIDTH = IMG_WIDTH;
+        this.IMG_HEIGHT = IMG_HEIGHT;
+    }
+
+   
+    
+    public int getIMG_WIDTH() {
+        return IMG_WIDTH;
+    }
+
+    public int getIMG_HEIGHT() {
+        return IMG_HEIGHT;
+    }
+
+    public void setIMG_WIDTH(int IMG_WIDTH) {
+        this.IMG_WIDTH = IMG_WIDTH;
+    }
+
+    public void setIMG_HEIGHT(int IMG_HEIGHT) {
+        this.IMG_HEIGHT = IMG_HEIGHT;
+    }
+    
+    
+    
+    
+}
