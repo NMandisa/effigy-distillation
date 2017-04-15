5891cfb4b901fd04e4cfa28e41407915628e4ce3
Noxolo.Mkhungo <Noxolo.Mkhungo@apparel-uk.local>
2017-04-15 13:30:03 +0200

    Commit-2017-04-04

--- src/com/effigy/distillation/ImageResizerHybrisPlugin.java
+++ src/com/effigy/distillation/ImageResizerHybrisPlugin.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package image.resizer.hybris.plugin;
+package com.effigy.distillation;
 import java.awt.AlphaComposite;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
