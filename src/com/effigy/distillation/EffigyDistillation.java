22a8daa4e16885e5151d99ba2ae5a7644ced227b
Noxolo Mandisa Mkhungo <NMandisa@users.noreply.github.com>
2023-11-16 02:16:17 +0200

    removed unused imported and template for license

--- src/com/effigy/distillation/EffigyDistillation.java
+++ src/com/effigy/distillation/EffigyDistillation.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.effigy.distillation;
 import java.awt.AlphaComposite;
 import java.awt.Dimension;
@@ -11,8 +6,6 @@ import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import javax.imageio.ImageIO;
 
 
